17b6f07783dfee6df80d584a3a9f7d4c5eb42e14
tanachok12 <tanachok_d@elearning.ac.th>
2022-07-31 16:15:42 +0700

    3.2

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '../views/EventListView.vue'
 import AboutView from '../views/AboutView.vue'
-import EventDetails from '@/views/EventDetailView.vue'
+import EventDetailView from '@/views/event/EventDetailView.vue'
+import EventRegisterView from '@/views/event/EventRegister.vue'
+import EventEditView from '@/views/event/EventEditView.vue'
+import EventLayoutView from '@/views/event/EventLayoutView.vue'
 const routes = [
   {
     path: '/',
@@ -19,9 +22,29 @@ const routes = [
   },
   {
     path: '/event/:id',
-    name: 'EventDetails',
-    component: EventDetails,
-    props: true
+    name: 'EventLayout',
+    props: true,
+    component: EventLayoutView,
+    children: [
+      {
+        path: '',
+        name: 'EventDetails',
+        component: EventDetailView,
+        props: true
+      },
+      {
+        path: 'register',
+        name: 'EventRegister',
+        component: EventRegisterView,
+        props: true
+      },
+      {
+        path: 'edit',
+        name: 'EventEdit',
+        component: EventEditView,
+        props: true
+      }
+    ]
   }
 ]
 
